260c2306781346bc93d90e346cca247604790e7b
Vojtech Lengal <vojta.lengal@gmail.com>
2021-06-14 21:24:02 +0200

    second test script added

--- test_scripts/test02.sql
+++ test_scripts/test02.sql
@@ -19,11 +19,53 @@ select * from AccountsWithOwners;
 select * from ActiveLoansWithOwners;
 
 -- loan the money (transfer 10000 to account with Id 1)
-exec AddLoan 1,10000,1000;
+exec AddLoan 1,10000,5000;
 
 select * from AccountsWithOwners;
 select * from ActiveLoansWithOwners;
 
-exec GetActiveLoansOf 2;
-exec TotalMonthlyPaymentOf 2;
+-- pay the loan
+exec PayMonthlyLoan 1,3;
+exec PayMonthlyLoan 1,3;
+exec PayMonthlyLoan 1,3;
 
+select * from ActiveLoansWithOwners;
+select * from AccountsWithOwners;
+
+-- mark the completely paid loans as completed
+exec MarkCompletedLoans;
+select * from ActiveLoansWithOwners;
+
+-- MONEY TRANSFERS
+
+-- deposit money
+select * from AccountsWithOwners;
+exec DepositMoney 1,500;
+select * from AccountsWithOwners;
+
+-- withdraw some money
+exec WithdrawMoney 2,1000;
+exec WithdrawMoney 3,50000;
+
+select * from AccountsWithOwners;
+
+-- transfer money
+exec TransferMoney 1,2,2500;
+exec TransferMoney 3,1,2500;
+exec TransferMoney 5,4,10000;
+exec TransferMoney 5,4,15000;
+
+select * from AccountsWithOwners;
+select * from AllTransactions;
+
+-- transfer another money
+exec TransferMoney 2,3,250;
+exec TransferMoney 5,2,400;
+exec TransferMoney 4,1,10000;
+exec TransferMoney 4,2,6666;
+
+select * from AccountsWithOwners;
+select * from AllTransactions;
+
+exec GetAllIncomingTransactions 2;
+exec GetAllOutcomingTransactions 2;

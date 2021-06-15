dc27c59cc4d96beda603917abd56a6e3ea845f15
Vojtech Lengal <vojta.lengal@gmail.com>
2021-06-15 10:09:14 +0200

    update_statistics added

---
+++ update_statistics.sql
@@ -0,0 +1,15 @@
+-- update all statistics
+update statistics [dbo].[CommonData];
+update statistics [dbo].[People];
+update statistics [dbo].[Departments];
+update statistics [dbo].[Employees];
+update statistics [dbo].[Clients];
+update statistics [dbo].[Loans];
+update statistics [dbo].[Accounts];
+update statistics [dbo].[PaymentCards];
+update statistics [dbo].[MoneyTransactions];
+update statistics [dbo].[LoanPayments];
+update statistics [dbo].[YearlyInterestTransactions];
+update statistics [dbo].[MoneyWithdrawals];
+update statistics [dbo].[MoneyDeposits];
+update statistics [dbo].[MoneyTransfers];
